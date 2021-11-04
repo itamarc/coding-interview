46588dd5b5a9cccf119857d7fbcee363e3fc936d
Itamar Carvalho <itamarc@gmail.com>
2021-11-04 12:23:20 -0300

    Initial commit with Maven project

---
+++ src/main/java/io/github/itamarc/Sort.java
@@ -0,0 +1,9 @@
+package io.github.itamarc;
+
+public class Sort {
+    public float[] someSortAlgorithm(float[] in) {
+        float[] out = new float[in.length];
+        // implement the sorting logic
+        return out;
+    }
+}
