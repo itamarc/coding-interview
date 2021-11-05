821023de699051d2a2e2bed5293ff12a8c0ce8f3
Itamar Carvalho <itamarc@gmail.com>
2021-11-05 19:03:23 -0300

    Regex in Java

--- src/main/java/io/github/itamarc/App.java
+++ src/main/java/io/github/itamarc/App.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class App {
     public static void main( String[] args ) {
         iterate();
+        Regex.findGroup();
     }
 
     private static  void attribution() {
