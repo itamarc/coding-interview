c863e9238133f66f432b3ddb922ff42f2c1ecf2d
Itamar Carvalho <itamarc@gmail.com>
2021-11-05 21:07:29 -0300

    Merge sort

--- src/main/java/io/github/itamarc/App.java
+++ src/main/java/io/github/itamarc/App.java
@@ -12,6 +12,10 @@ public class App {
     public static void main( String[] args ) {
         iterate();
         Regex.findGroup();
+
+        float[] farr = {12F, 1.1F, 10F, -0.4F, 2.2F, 0.3F, 0F, -13F, 3.3F, 0.2F, 11F, 14F, 15F, -4.4F, -0.1F, 5.5F};
+        System.out.println("Original array:\n" + Arrays.toString(farr));
+        System.out.println("Merge sorted array:\n" + Arrays.toString(Sort.topDownMergeSort(farr)));
     }
 
     private static  void attribution() {
