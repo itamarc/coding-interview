43f59326f800c178d051060dad75d4c5c1ae52be
Itamar Carvalho <itamarc@gmail.com>
2022-02-17 17:34:27 -0300

    Regexp changing a group to lowercase

--- src/main/java/io/github/itamarc/InAndOut.java
+++ src/main/java/io/github/itamarc/InAndOut.java
@@ -1,6 +1,5 @@
 package io.github.itamarc;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class InAndOut {
--- src/main/java/io/github/itamarc/Regex.java
+++ src/main/java/io/github/itamarc/Regex.java
@@ -4,7 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static void findGroup() {
+
+    public static void main(String[] args) {
+        findGroup();
+        renameTestMethod();
+    }
+
+    private static void findGroup() {
         String text = "abcde32[a3[t]b]cd10[xyz]3[k]";
         System.out.println("Text: " + text);
         String expr = "(\\d+)\\[";
@@ -14,5 +20,21 @@ public class Regex {
         while(m.find()) {
             System.out.println("Found '" + m.group(1) + "' on position " + m.start());
         }
+    }
+
+    private static void renameTestMethod() {
+        String name = """
+        @Test
+        public void testMethod(
+            """;
+
+        Pattern p = Pattern.compile("^\\s*@Test\\s*\n\\s*(.+)\\s+test(\\w)(\\w+)\\s*\\(.*");
+        Matcher m = p.matcher(name);
+        if (m.find()) {
+            System.out.println("Found:\n@Test\n" + m.group(1) + " " + m.group(2).toLowerCase() + m.group(3) + "(");
+        } else {
+            System.out.println("Not found");
+        }
+
     }
 }
